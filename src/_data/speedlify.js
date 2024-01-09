2bf7f7f7f98143550e488a9fa7248379c769b943
Daniele T <Fupete@users.noreply.github.com>
2024-01-09 22:58:44 +0100

    Update speedlify.js

--- src/_data/speedlify.js
+++ src/_data/speedlify.js
@@ -1,3 +1,5 @@
+const ElevetyFetch = require('@11ty/eleventy-fetch');
+
 module.exports = async function() {
 	let url = "https://iridescent-squirrel-c9bbac.netlify.app/api/urls.json";
 	let json = await EleventyFetch(url, {
