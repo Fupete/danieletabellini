a5e35591392e6cd76e745ce03f1264c0cf7ae5d1
Daniele T <Fupete@users.noreply.github.com>
2024-01-09 13:12:35 +0100

    fix: typo

--- src/assets/js/modules/speedlify.js
+++ src/assets/js/modules/speedlify.js
@@ -1,5 +1,3 @@
-// const CacheAsset = require("@11ty/eleventy-cache-assets");
-
 export default async function() {
 	let url = "https://iridescent-squirrel-c9bbac.netlify.app/api/urls.json";
 	let json = await EleventyFetch(url, {
