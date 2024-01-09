3e012071da22fc91d4834aca76886808f92c616b
Daniele T <Fupete@users.noreply.github.com>
2024-01-09 22:56:35 +0100

    feat: Create speedlify.js

---
+++ src/_data/speedlify.js
@@ -0,0 +1,9 @@
+module.exports = async function() {
+	let url = "https://iridescent-squirrel-c9bbac.netlify.app/api/urls.json";
+	let json = await EleventyFetch(url, {
+		duration: "1d",
+		type: "json",
+	});
+
+	return json;
+};
