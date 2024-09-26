dc41bb778f0df6ab409c02de0274bfebeb988a15
Fupete <Fupete@users.noreply.github.com>
2024-09-26 10:04:23 +0000

    deploy: b6c6616a3b0c8a0847830c514c73ed78ca8be0c3

--- assets/js/pagefind-4ed993c7.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=pagefind-4ed993c7.js.map
---
+++ assets/js/pagefind-7a62f35d.js
@@ -0,0 +1,2 @@
+import"/pagefind/pagefind-ui.js";const e=()=>{new PagefindUI({element:"#searchBox",showImages:!0,showEmptyFilters:!0,bundlePath:"/pagefind/"}),document.querySelector("input").setAttribute("id","site-search")},t=document.querySelector(".home");t&&e();
+//# sourceMappingURL=pagefind-7a62f35d.js.map
